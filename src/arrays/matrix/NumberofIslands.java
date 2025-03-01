33893c57408307920d51dc4bb20f55429f3e2ee5
Garya Nimgirov <nimgirov@mail.ru>
2025-03-02 01:56:37 +0300

    1

---
+++ src/arrays/matrix/NumberofIslands.java
@@ -0,0 +1,92 @@
+package arrays.matrix;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class NumberofIslands {
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println(numIslands(grid));
+    }
+
+    public static int numIslands(char[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        return funcDfs(grid, 0, 0, visited, 0);
+
+    }
+
+    private static int funcDfs(char[][] grid, int i, int j, boolean[][] visited, int count) {
+        while (getNextStartOfIsland(grid, i, j, visited) != null) {
+            Holder holder = funcDfs(grid, i, j, visited);
+            visited = holder.visited;
+            i = holder.idxArr[0];
+            j = holder.idxArr[1];
+            count++;
+        }
+        return count;
+    }
+
+    private static Holder funcDfs(char[][] grid, int i, int j, boolean[][] visited) {
+        int[] idxArr = getNextStartOfIsland(grid, i, j, visited);
+        if (idxArr == null) {
+            return new Holder(visited, null);
+        }
+        visited[idxArr[0]][idxArr[1]] = true;
+
+        int[][] neighbours = getNeighbours(idxArr[0], idxArr[1], grid.length, grid[0].length);
+
+        List<Holder> holders = new ArrayList<>();
+        for (int[] neighbour : neighbours) {
+            if (neighbour == null || grid[neighbour[0]][neighbour[1]] == '0') {
+                continue;
+            }
+            if (!visited[neighbour[0]][neighbour[1]]) {
+                Holder holder = funcDfs(grid, neighbour[0], neighbour[1], visited);
+                holders.add(holder);
+            }
+        }
+        return holders.isEmpty() ? new Holder(visited, idxArr) : holders.stream().max(Comparator.comparingInt(a -> a.idxArr[1])).orElse(null);
+    }
+
+    private static class Holder {
+        boolean[][] visited;
+        int[] idxArr;
+
+        public Holder(boolean[][] visited, int[] idxArr) {
+            this.visited = visited;
+            this.idxArr = idxArr;
+        }
+    }
+
+    private static int[] getNextStartOfIsland(char[][] grid, int i, int j, boolean[][] visited) {
+        if (!visited[i][j] && grid[i][j] == '1') {
+            return new int[]{i, j};
+        }
+        for (int l = 0; l < grid.length; l++) {
+            for (int k = 0; k < grid[0].length; k++) {
+                if (!visited[l][k] && grid[l][k] == '1') {
+                    return new int[]{l, k};
+                }
+            }
+        }
+        return null;
+    }
+
+    private static int[][] getNeighbours(int i, int j, int m, int n) {
+        int[][] r = new int[4][2];
+        r[0] = i + 1 < m ? new int[]{i + 1, j} : null;
+        r[1] = j + 1 < n ? new int[]{i, j + 1} : null;
+        r[2] = i - 1 >= 0 ? new int[]{i - 1, j} : null;
+        r[3] = j - 1 >= 0 ? new int[]{i, j - 1} : null;
+        return r;
+    }
+
+}
---
+++ src/math/ClimbingStairs.java
@@ -0,0 +1,65 @@
+package math;
+
+public class ClimbingStairs {
+
+    /*
+        Example 1:
+        Input: n = 2
+        Output: 2
+        Explanation: There are two ways to climb to the top.
+        1. 1 step + 1 step
+        2. 2 steps
+
+        Example 2:
+        Input: n = 3
+        Output: 3
+        Explanation: There are three ways to climb to the top.
+        1. 1 step + 1 step + 1 step
+        2. 1 step + 2 steps
+        3. 2 steps + 1 step
+
+        Example 3:
+        Input: n = 4
+        1. 1 + 1 + 1 + 1
+        2. 1 + 1 + 2
+        3. 1 + 2 + 1
+        4. 2 + 1 + 1
+        5. 2 + 2
+        Output: 5
+
+        Example 4:
+        Input: n = 5
+        1. 1 + 1 + 1 + 1 + 1
+        2. 1 + 1 + 1 + 2
+        3. 1 + 1 + 2 + 1
+        4. 1 + 2 + 1 + 1
+        5. 2 + 1 + 1 + 1
+        6. 1 + 2 + 2
+        7. 2 + 2 + 1
+        8. 2 + 1 + 2
+        Output: 8
+     */
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climbStairs(n));
+    }
+
+    public static int climbStairs(int n) {
+        int step1 = 1;
+        int step2 = 2;
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        for (int i = 3; i <= n; i++) {
+            int tmp = step2;
+            step2 = step1 + step2;
+            step1 = tmp;
+        }
+        return step2;
+    }
+
+}
