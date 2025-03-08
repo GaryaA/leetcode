820c9ce8b991ad0670e4f518dd74f48e4c77a83c
Garya Nimgirov <nimgirov@mail.ru>
2025-03-09 00:57:11 +0300

    1

--- src/lists/PeekingIterator.java
+++ src/lists/PeekingIterator.java
@@ -1,4 +1,4 @@
-package arrays;
+package lists;
 
 import java.util.Iterator;
 import java.util.LinkedList;
---
+++ src/math/BasicCalculatorII.java
@@ -0,0 +1,41 @@
+package math;
+
+import java.util.Stack;
+
+public class BasicCalculatorII {
+
+    public static void main(String[] args) {
+        String s = "3+2*2";
+        System.out.println(new BasicCalculatorII().calculate(s));
+    }
+
+    public int calculate(String s) {
+        int number = 0;
+        char operator = ' ';
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isSpaceChar(c) && i != s.length() - 1) continue;
+
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
+                if (i != s.length() - 1) continue;
+            }
+
+            switch (operator) {
+                case '-' -> stack.push(-number);
+                case '*' -> stack.push(stack.pop() * number);
+                case '/' -> stack.push(stack.pop() / number);
+                default -> stack.push(number);
+            }
+            operator = c;
+            number = 0;
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+
+}
